a74927bf83398d62342096ddf04f3d976ea7f745
Shem Magnezi <shem@shems-mbp.mynet>
2016-09-16 15:26:58 +0300

    Use in code click listerners

--- app/src/main/java/shem/com/materiallogin/ex/MainActivity.java
+++ app/src/main/java/shem/com/materiallogin/ex/MainActivity.java
@@ -1,8 +1,10 @@
 package shem.com.materiallogin.ex;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,17 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void defaultView(View view) {
-        startActivity(new Intent(this, DefaultActivity.class));
-    }
+        findViewById(R.id.default_dialog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DefaultActivity.class));
+            }
+        });
 
-    public void customizedView(View view) {
-        startActivity(new Intent(this, CustomizeActivity.class));
-    }
+        findViewById(R.id.custom_dialog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CustomizeActivity.class));
+            }
+        });
 
-    public void customViews(View view) {
-        startActivity(new Intent(this, CustomViewsActivity.class));
+        findViewById(R.id.custom_views).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CustomViewsActivity.class));
+            }
+        });
     }
 }
